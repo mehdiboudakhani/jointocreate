8e022cf90951b0e157540bafc9fd7ef9bcfb41e6
Mehdi <169154093+mehdiboudakhani@users.noreply.github.com>
2026-02-21 17:55:38 +0100

    feat: apply migrations automatically at startup

--- src/JTC/Program.cs
+++ src/JTC/Program.cs
@@ -6,12 +6,17 @@
     class Program
     {
         /// <summary>
-        /// Configures dependency injection and starts the bot.
+        /// Configures dependency injection, applies database migrations and starts the bot.
         /// </summary>
         /// <returns>A task representing the asynchronous operation.</returns>
         static async Task Main()
         {
             IServiceProvider serviceProvider = DependencyInjection.BuildServiceProvider();
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var database = scope.ServiceProvider.GetRequiredService<Database>();
+                database.Database.Migrate();
+            }
             await serviceProvider.GetRequiredService<Bot>().RunAsync();
         }
     }
